5e4c9a76d2e3d888a22e18ef237e3fff6e51e708
johnny <626142589@qq.com>
2020-11-14 18:02:25 +0800

    Hello

---
+++ src/main/java/HelloDeepin.java
@@ -0,0 +1,10 @@
+/**
+ * @author johnny
+ * @create 2020-11-14 下午5:58
+ **/
+public class HelloDeepin {
+
+    public static void main(String[] args) {
+        System.out.println("Hello ");
+    }
+}
